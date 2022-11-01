821e6d852fd61e58e40f2003ea9bd1f0506df4ff
danbloch21~ <73940682+danbloch21@users.noreply.github.com>
2022-11-01 21:24:30 +0200

    modified index.js a second time

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT;
+const redirectUri = "http://localhost:8888/callback";
 
 app.post("/login", async (req, res) => {
   const { code } = req.body;
