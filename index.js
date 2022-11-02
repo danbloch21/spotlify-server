f70752c9c2b99b79551930515b3e4c7a5749fe30
danbloch21~ <73940682+danbloch21@users.noreply.github.com>
2022-11-02 13:42:42 +0200

    amended const PORT in server index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 3000;
 
 app.post("/login", async (req, res) => {
   const { code } = req.body;
